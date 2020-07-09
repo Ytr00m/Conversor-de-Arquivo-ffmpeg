0f3d2008cfe9eafb3487e7cb7780e0cb289d7597
BuildTools <unconfigured@null.spigotmc.org>
2020-07-09 16:40:42 -0300

    Programa v1.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Conversor-de-Arquivo-ffmpeg
-Programa em python para converter conjuntos de arquivos ffmpeg
+# Conversor de ffmpeg em python3
+
+Este é um programa para converter arquivos de musica/videos para diversos formatos suportados pelo ffmpeg escalável.
+
+## Certifique de ter python3 instalado
+```bash
+sudo apt install python3
+```
+
+## Como usar
+
+```bash
+python3 archive_converter.py
+```
+
+## Como contribuir
+Sinta-se à vontade para mandar pull request ou abrir uma issue.
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ archive_convertor.py
@@ -0,0 +1,39 @@
+import os
+
+def ffmpeg():
+    if not os.path.isfile('/usr/bin/ffmpeg'):
+        print("O ffmpeg não está instalado, mas vou instalar pra você!\n")
+        os.system("sudo apt install ffmpeg")
+        os.system("\n")
+        print("\nAgora está tudo pronto!\n")   
+
+def separaArqvs(lista, newlist, format):
+    for f in lista:
+        if f.endswith(format_ori):
+            newlist.append(f)
+    return newlist
+
+
+def converter(diretorio, format_ori, format_final):
+    os.chdir(diretorio)
+    tmparqvs = os.listdir(diretorio)
+    arqvs = []
+    arqvs = separaArqvs(tmparqvs, arqvs, format_ori)
+    os.mkdir("Convertidos")
+    os.mkdir("Originais")
+    for i in arqvs:
+        arqv_ori = i.replace(' ', '\ ')
+        arqv_final = arqv_ori.replace(format_ori, format_final)
+        os.system(f"ffmpeg -i {arqv_ori} {arqv_final}")
+        os.system(f"mv {arqv_final} Convertidos")
+        os.system(f"mv {arqv_ori} Originais")
+
+
+ffmpeg()
+        
+diretorio = input("Digite onde está os arquivos: ")
+format_ori = input("Digite o formato atual dos arquivos: ")
+format_final = input("Digite o formato final: ")
+
+converter(diretorio, format_ori, format_final)
+print("\nAcabado!!\n")
